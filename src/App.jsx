f72d94682c041e471c196f49cda70e81269aaf23
Zeke-Cachi <ezequielcachi@gmail.com>
2023-04-25 19:55:46 -0300

    Se agrega un poco de estilos

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,7 @@ import 'tailwindcss/tailwind.css';
 import { useContext, useState } from "react";
 import CartContext from "./Components/Contexts/CartContext";
 import Cart from "./Components/Cart/Cart";
-import { Transition } from '@headlessui/react'
-import { Dialog } from '@headlessui/react';
+import { FaTimes } from "react-icons/fa";
 
 
 
@@ -27,26 +26,36 @@ const slides = [
 ];
 
 function App() {
-  const { products, isShowing, setIsShowing, cart } = useContext(CartContext);
 
+  const { cart, products, isShowing, setIsShowing } = useContext(CartContext);
+  
 
   return (
     <div className="bg-gray-800">
-      <>
-        <FaShoppingCart
-          className="h-7 w-7 absolute top-10 right-10 z-50 text-white cursor-pointer"
-          onClick={() => {
-            setIsShowing((isShowing) => !isShowing);
-          }}
-        />
-          {isShowing && cart.length > 0 ?
-            <Cart /> : 
-            isShowing && cart.length === 0 ? 
-            <div className="bg-red-500">No items found!</div> :
-            null
-            }
+      <div>
+        <FaShoppingCart className="h-[1.75rem] w-[1.75rem] absolute top-[2.5rem] right-[2.5rem] z-50 text-white cursor-pointer" onClick={() => {
+          setIsShowing((isShowing) => !isShowing)}} />
 
-      </>
+        {cart.length > 0 ? 
+        <div className="h-[1.5rem] w-[1.5rem] absolute top-[1.5rem] right-[1.5rem] z-50 font-bold text-white text-center bg-red-500 rounded-full cursor-pointer">{cart.length}</div> : null}
+
+        {isShowing && cart.length > 0 ? 
+        <Cart /> : 
+        isShowing && cart.length === 0 ? (
+          <div className="relative">
+            <div className="absolute w-[13rem] h-[8rem] md:w-[15rem] md:h-[9rem] lg:w-[20rem] lg:h-[11rem] bg-gray-800 mx-auto top-[8rem] left-1/2 transform -translate-x-1/2 
+            z-40 rounded-md border-2 border-gray-500 text-white font-bold text-[1.3rem] md:text-[1.7rem] lg:text-[2.1rem] grid place-items-center">No items found!
+
+                <FaTimes className="absolute text-white z-50 h-[10rem] w-[.7rem] top-[-4rem] right-[.5rem] cursor-pointer" onClick={ () => {
+                    setIsShowing((isShowing) => !isShowing)
+                }} />
+            </div>
+          </div>
+        ) : null}
+
+
+    </div>
+
 
       <Carousel slides={slides} />
 
--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -4,16 +4,20 @@ import { useContext } from 'react';
 import { CartContext } from '../Contexts/CartContext';
 import { FaTimes } from "react-icons/fa";
 
+
 const Cart = () => {
 
-    const { cart, deleteFromCart, clearCart, addOneFromCart, setIsShowing } = useContext(CartContext);
+    const { cart, deleteFromCart, clearCart, addOneFromCart, isShowing, setIsShowing } = useContext(CartContext);
 /* FUNCION PARA CALCULAR TOTAL*/
 const totals = cart.reduce((acc, curr) => {
     return acc + curr.quantity * curr.price;
 }, 0);
 /* FIN FUNCION CALCULAR TOTAL*/
+
+
+
     return (
-        
+
         <div id="cart" className="relative">
             <div className="absolute w-10/12 min-h-300p w-1/3 bg-gray-800 mx-auto top-20 left-1/2 transform -translate-x-1/2 
             z-40 rounded-md w-5/6 border-2 border-gray-500">
@@ -24,13 +28,19 @@ const totals = cart.reduce((acc, curr) => {
                 
                 <h1 className="text-4xl text-center py-1 font-Roboto text-white">Carrito</h1> 
                 
-                <div>{cart.map((item) => {return <ItemCart key={item.id} data={item} deleteFromCart={deleteFromCart} addOneFromCart={addOneFromCart} />})}</div>
+                <div>{cart.map((item) => {return <ItemCart totals={totals} key={item.id} data={item} deleteFromCart={deleteFromCart} addOneFromCart={addOneFromCart} />})}</div>
 
                 <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 mx-8 text-center gap-2">
-                <h5 className='text-white'>Total: ${totals}</h5>
-                    <button className="bg-yellow-300 text-black font-bold py-1 px-4 rounded my-8 cursor-pointer " 
-                    onClick={() => clearCart()}>Limpiar Carrito</button>
-                    <button className="bg-yellow-300 text-black font-bold py-1 px-4 rounded my-8 cursor-pointer ">Pagar</button>
+                    <h5 className='text-white text-[1.4rem] text-yellow-300 text-bold col-span-full text-center'>Total: $ {totals}</h5>
+                    <div className="flex flex-col items-center sm:flex-row justify-center col-span-6 gap-[3rem]">
+                        <button className="bg-yellow-300 text-black font-bold py-1 px-4 rounded my-8 cursor-pointer h-[2rem] w-[10rem] sm:me-[1rem]" 
+                        onClick={() => clearCart()}>Limpiar Carrito</button>
+                        <button className="bg-yellow-300 text-black font-bold py-1 px-4 rounded my-8 cursor-pointer h-[2rem] w-[10rem] sm:ms-[1rem]" onClick={() => {
+                            alert('Payment successfull');
+                            setIsShowing(isShowing => !isShowing);
+                            clearCart()
+                        }}>Pagar</button>
+                    </div>
                 </div>
 
             </div>

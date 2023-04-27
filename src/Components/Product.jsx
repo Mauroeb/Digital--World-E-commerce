5128424c1422eebf00153072dca13e7c9eb7b018
rosmildoLopes <[200~123316625+rosmildoLopes@users.noreply.github.com~>
2023-04-26 21:21:17 -0300

    Update styles

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -7,14 +7,14 @@ const Product = ({ product }) => {
   const { id, image, name, description, price } = product;
 
   return (
-    <div className="w-full p-3 bg-white rounded-lg relative shadow-lg">
+    <div className="w-full p-2 hover:drop-shadow-2xl bg-white rounded-lg relative shadow-lg">
       <div
         className="flex justify-center items-center 
         overflow-hidden group transition container mx-auto"
       >
         <div className="w-full flex justify-center items-center">
           {/* image */}
-          <div className="w-full h-64 object-cover p-8 flex mx-auto justify-center items-center bg-zinc-200 rounded-lg">
+          <div className="w-full h-64 object-cover p-6 flex mx-auto justify-center items-center bg-zinc-200 rounded-lg">
             <img
               className="group-hover:scale-110 transition duration-300"
               src={product.image}
@@ -24,20 +24,21 @@ const Product = ({ product }) => {
         </div>
       </div>
       {/* name , description , price */}
-      <div className="mt-3 flex flex-col">
+      <div className="mt-3 flex flex-col mx-2">
         <Link to={`/product/${id}`}>
           <h2 className="font-semibold mb-1 text-xl">{product.name}</h2>
         </Link>
         <div className="mt-1 text-slate-400">{product.description}</div>
-        <div className="font-bold mt-8 text-2xl">$ {product.price}</div>
+        
       </div>
 
       {/* buttons */}
       <div>
-        <div className="">
+        <div className="flex justify-between items-center mt-10 mx-1">
+          <div className="font-bold  text-2xl bottom-5">$ {product.price}</div>
           <button
-            className="bg-zinc-800 hover:bg-zinc-950 text-white font-bold
-         p-2 px-4 bottom-3 right-3 flex items-center justify-center absolute rounded-xl"
+            className="bg-zinc-800 text-white hover:bg-zinc-950 font-bold
+         p-2 px-4  flex items-center justify-center rounded-xl"
           >
             Add to Cart
           </button>
